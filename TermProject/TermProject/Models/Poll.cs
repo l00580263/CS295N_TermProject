d29179cb473c8c8c4f718f3cc71dd9aecb047ace
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-11-29 18:33:04 -0800

    Cleaned template, added models

---
+++ TermProject/TermProject/Models/Poll.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TermProject.Models
+{
+    public class Poll
+    {
+
+        #region Properties
+        public int PollId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<PollOption> Options { get; set; }
+        public DateTime EndDate { get; set; }
+        #endregion
+
+
+
+        #region Methods
+        public string GetResult()
+        {
+            if (!IsPollClosed())
+            {
+                return "Poll Hasn't Closed.";
+            }
+
+
+            // order votes
+            List<PollOption> sortedVotes = Options.OrderByDescending(v => v.Votes).ToList();
+
+            if (sortedVotes[0] != sortedVotes[1])
+            {
+                // give winner
+                return string.Format("{0} has won the vote.", sortedVotes[0].Name);
+            }
+            else
+            {
+                // tied
+                return "The Poll has ended in a tie";
+            }
+        }
+
+
+
+        public bool IsPollClosed()
+        {
+            if (EndDate.Ticks > DateTime.Now.Ticks)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ TermProject/TermProject/Models/PollOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TermProject.Models
+{
+    public class PollOption
+    {
+        public string Name { get; set; }
+        public int Votes { get; set; }
+    }
+}
