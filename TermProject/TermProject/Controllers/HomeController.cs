ce8703ea35247ff6f375f3852b033a830c1b1438
unknown <you@example.comdedorec2@my.lanecc.edu>
2018-12-02 13:13:39 -0800

    Completed Activity page

--- TermProject/TermProject/Controllers/HomeController.cs
+++ TermProject/TermProject/Controllers/HomeController.cs
@@ -110,5 +110,69 @@ namespace TermProject.Controllers
 
             return View("Vote");
         }
+
+
+
+        public IActionResult Activity()
+        {
+            // Poll Data
+            // add poll count to bag
+            ViewBag.PollCount = repo.Polls.Count();
+
+            // add open poll count to bag
+            ViewBag.OpenPollCount = (from p in repo.Polls
+                                     where !p.IsPollClosed()
+                                     select p).Count();
+
+            // add first poll to bag
+            ViewBag.OldestPoll = repo.Polls.First();
+
+            // add last poll to bag
+            ViewBag.NewestPoll = repo.Polls.Last();
+
+
+            // Vote Data
+            // add vote count to bag
+            ViewBag.VoteCount = repo.PollOptions.Sum(o => o.Votes);
+
+            // get total votes per poll
+            var pollVoteTotals = (from Poll p in repo.Polls
+                                  select p.Options.Sum(o => o.Votes)).ToList();
+
+            // add most votes to bag
+            ViewBag.MostVotes = pollVoteTotals.Max();
+
+            // add least votes to bag
+            ViewBag.LeastVotes = pollVoteTotals.Min();
+
+            // add most voted poll to bag
+            ViewBag.MostVotedPoll = (from p in repo.Polls
+                                     where p.Options.Sum(o => o.Votes) == pollVoteTotals.Max()
+                                     select p).First();
+
+            // add least voted poll to bag
+            ViewBag.LeastVotedPoll = (from p in repo.Polls
+                                     where p.Options.Sum(o => o.Votes) == pollVoteTotals.Min()
+                                     select p).First();
+
+
+            // Option Data
+            // add option count to bag
+            ViewBag.OptionCount = repo.PollOptions.Count();
+
+            // add most options on a poll to bag
+            ViewBag.MostOptions = repo.Polls.Max(p => p.Options.Count());
+
+            // add least options on a poll to bag
+            ViewBag.LeastOptions = repo.Polls.Min(p => p.Options.Count());
+
+            // add most votes on an option to bag
+            ViewBag.MostOptionVotes = repo.PollOptions.Max(p => p.Votes);
+
+            // add most votes on an option to bag
+            ViewBag.LeastOptionVotes = repo.PollOptions.Min(p => p.Votes);
+
+            return View();
+        }
     }
 }
